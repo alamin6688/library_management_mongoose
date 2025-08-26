cc5d73ef1a15c54dd562b741eb2bccba232399eb
Alamin <alamin.kzs06@gmail.com>
2025-08-26 15:51:48 +0600

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome To Library Management App!!!");
+});
+
+export default app;
